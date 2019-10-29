1073fba8e81a29dc57c7190c962c6ec965dfd76c
Diego Rojo <diego.rojo@tsoftglobal.com>
2019-10-29 15:35:53 -0300

    Ajustes

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,8 @@ namespace backend
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            //services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_0);
 
             // services.AddDbContext<Context>(opts => opts.UseSqlServer(Configuration["ConnectionString:EmployeeDB"]));
 
